d40cbbf10e6f155d7d5e248beb00d6ed9a5f3b79
SheilaAguirreRivas <sheyba0803@gmail.com>
2023-07-10 13:49:53 -0300

    Cambios para PyAW

--- js/eventos.js
+++ js/eventos.js
@@ -4,9 +4,9 @@ createApp({
   data() {
     return {
       eventos: [],
-      url: 'http://127.0.0.1:5000/eventos',
+      url: 'http://sheiaguirrer.pythonanywhere.com/eventos',
       //'http://127.0.0.1:5000/eventos',
-      //'http://sheiaguirrer.pythonanywhere.com/eventos',
+      //'http://sheiaguirrer.pythonanywhere.com/eventos';
       cargando: true,
       error: false
     };
